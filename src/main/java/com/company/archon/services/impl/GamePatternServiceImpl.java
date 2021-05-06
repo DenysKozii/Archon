d319266a02164b9e2fea36a33ca757833337fc59
DenysKozii <denys.kozii@gmail.com>
2021-05-06 19:50:38 +0300

    fix condition

--- src/main/java/com/company/archon/services/impl/GamePatternServiceImpl.java
+++ src/main/java/com/company/archon/services/impl/GamePatternServiceImpl.java
@@ -1,7 +1,10 @@
 package com.company.archon.services.impl;
 
 import com.company.archon.dto.GamePatternDto;
-import com.company.archon.entity.*;
+import com.company.archon.entity.ConditionParameter;
+import com.company.archon.entity.GamePattern;
+import com.company.archon.entity.User;
+import com.company.archon.entity.UserParameter;
 import com.company.archon.exception.EntityNotFoundException;
 import com.company.archon.mapper.GamePatternMapper;
 import com.company.archon.pagination.PageDto;
@@ -20,7 +23,6 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Comparator;
 import java.util.List;
-import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 @Service
@@ -58,83 +60,83 @@ public class GamePatternServiceImpl implements GamePatternService {
         parameter0.setGamePattern(gamePattern);
         conditionParameterRepository.save(parameter0);
 
-        ConditionParameter parameter1 = new ConditionParameter();
-        parameter1.setTitle("Талант к магии");
-        parameter1.setValue(0);
-        parameter1.setGamePattern(gamePattern);
-        conditionParameterRepository.save(parameter1);
-
-        ConditionParameter parameter2 = new ConditionParameter();
-        parameter2.setTitle("Смышленность");
-        parameter2.setValue(0);
-        parameter2.setGamePattern(gamePattern);
-        conditionParameterRepository.save(parameter2);
-
-        ConditionParameter parameter3 = new ConditionParameter();
-        parameter3.setTitle("Призрение к миру");
-        parameter3.setValue(0);
-        parameter3.setGamePattern(gamePattern);
-        conditionParameterRepository.save(parameter3);
-
-        ConditionParameter parameter4 = new ConditionParameter();
-        parameter4.setTitle("Талант к эфиру");
-        parameter4.setValue(0);
-        parameter4.setGamePattern(gamePattern);
-        conditionParameterRepository.save(parameter4);
-
-        ConditionParameter parameter5 = new ConditionParameter();
-        parameter5.setTitle("Талант к духу");
-        parameter5.setValue(0);
-        parameter5.setGamePattern(gamePattern);
-        conditionParameterRepository.save(parameter5);
-
-        ConditionParameter parameter6 = new ConditionParameter();
-        parameter6.setTitle("Харизма");
-        parameter6.setValue(0);
-        parameter6.setGamePattern(gamePattern);
-        conditionParameterRepository.save(parameter6);
-
-        ConditionParameter parameter7 = new ConditionParameter();
-        parameter7.setTitle("Интеллект");
-        parameter7.setValue(0);
-        parameter7.setGamePattern(gamePattern);
-        conditionParameterRepository.save(parameter7);
-
-        ConditionParameter parameter8 = new ConditionParameter();
-        parameter8.setTitle("Жестокость");
-        parameter8.setValue(0);
-        parameter8.setGamePattern(gamePattern);
-        conditionParameterRepository.save(parameter8);
-
-        ConditionParameter parameter9 = new ConditionParameter();
-        parameter9.setTitle("Манипуляции");
-        parameter9.setValue(0);
-        parameter9.setGamePattern(gamePattern);
-        conditionParameterRepository.save(parameter9);
-
-        ConditionParameter parameter10 = new ConditionParameter();
-        parameter10.setTitle("Единость с миром");
-        parameter10.setValue(0);
-        parameter10.setGamePattern(gamePattern);
-        conditionParameterRepository.save(parameter10);
-
-        ConditionParameter parameter11 = new ConditionParameter();
-        parameter11.setTitle("Правление");
-        parameter11.setValue(0);
-        parameter11.setGamePattern(gamePattern);
-        conditionParameterRepository.save(parameter11);
-
-        ConditionParameter parameter12 = new ConditionParameter();
-        parameter12.setTitle("Гениальность");
-        parameter12.setValue(0);
-        parameter12.setGamePattern(gamePattern);
-        conditionParameterRepository.save(parameter12);
-
-        ConditionParameter parameter13 = new ConditionParameter();
-        parameter13.setTitle("Кровожадность");
-        parameter13.setValue(0);
-        parameter13.setGamePattern(gamePattern);
-        conditionParameterRepository.save(parameter13);
+//        ConditionParameter parameter1 = new ConditionParameter();
+//        parameter1.setTitle("Талант к магии");
+//        parameter1.setValue(0);
+//        parameter1.setGamePattern(gamePattern);
+//        conditionParameterRepository.save(parameter1);
+//
+//        ConditionParameter parameter2 = new ConditionParameter();
+//        parameter2.setTitle("Смышленность");
+//        parameter2.setValue(0);
+//        parameter2.setGamePattern(gamePattern);
+//        conditionParameterRepository.save(parameter2);
+//
+//        ConditionParameter parameter3 = new ConditionParameter();
+//        parameter3.setTitle("Призрение к миру");
+//        parameter3.setValue(0);
+//        parameter3.setGamePattern(gamePattern);
+//        conditionParameterRepository.save(parameter3);
+//
+//        ConditionParameter parameter4 = new ConditionParameter();
+//        parameter4.setTitle("Талант к эфиру");
+//        parameter4.setValue(0);
+//        parameter4.setGamePattern(gamePattern);
+//        conditionParameterRepository.save(parameter4);
+//
+//        ConditionParameter parameter5 = new ConditionParameter();
+//        parameter5.setTitle("Талант к духу");
+//        parameter5.setValue(0);
+//        parameter5.setGamePattern(gamePattern);
+//        conditionParameterRepository.save(parameter5);
+//
+//        ConditionParameter parameter6 = new ConditionParameter();
+//        parameter6.setTitle("Харизма");
+//        parameter6.setValue(0);
+//        parameter6.setGamePattern(gamePattern);
+//        conditionParameterRepository.save(parameter6);
+//
+//        ConditionParameter parameter7 = new ConditionParameter();
+//        parameter7.setTitle("Интеллект");
+//        parameter7.setValue(0);
+//        parameter7.setGamePattern(gamePattern);
+//        conditionParameterRepository.save(parameter7);
+//
+//        ConditionParameter parameter8 = new ConditionParameter();
+//        parameter8.setTitle("Жестокость");
+//        parameter8.setValue(0);
+//        parameter8.setGamePattern(gamePattern);
+//        conditionParameterRepository.save(parameter8);
+//
+//        ConditionParameter parameter9 = new ConditionParameter();
+//        parameter9.setTitle("Манипуляции");
+//        parameter9.setValue(0);
+//        parameter9.setGamePattern(gamePattern);
+//        conditionParameterRepository.save(parameter9);
+//
+//        ConditionParameter parameter10 = new ConditionParameter();
+//        parameter10.setTitle("Единость с миром");
+//        parameter10.setValue(0);
+//        parameter10.setGamePattern(gamePattern);
+//        conditionParameterRepository.save(parameter10);
+//
+//        ConditionParameter parameter11 = new ConditionParameter();
+//        parameter11.setTitle("Правление");
+//        parameter11.setValue(0);
+//        parameter11.setGamePattern(gamePattern);
+//        conditionParameterRepository.save(parameter11);
+//
+//        ConditionParameter parameter12 = new ConditionParameter();
+//        parameter12.setTitle("Гениальность");
+//        parameter12.setValue(0);
+//        parameter12.setGamePattern(gamePattern);
+//        conditionParameterRepository.save(parameter12);
+//
+//        ConditionParameter parameter13 = new ConditionParameter();
+//        parameter13.setTitle("Кровожадность");
+//        parameter13.setValue(0);
+//        parameter13.setGamePattern(gamePattern);
+//        conditionParameterRepository.save(parameter13);
     }
 
     @Override
@@ -147,32 +149,31 @@ public class GamePatternServiceImpl implements GamePatternService {
         Page<GamePattern> result = gamePatternRepository.findAll(PagesUtility.createPageableUnsorted(page, pageSize));
 
         List<GamePatternDto> gamePatternDtos = mapGamePattern(result.getContent()).stream()
-                .filter(o->!o.getDeleted())
+                .filter(o -> !o.getDeleted())
                 .collect(Collectors.toList());
         List<String> availableTitles = mapGamePattern(available).stream()
                 .map(GamePatternDto::getTitle)
                 .collect(Collectors.toList());
 
         gamePatternDtos.stream()
-                .filter(gp->availableTitles.contains(gp.getTitle()))
-                .forEach(o->o.setAvailable(true));
+                .filter(gp -> availableTitles.contains(gp.getTitle()))
+                .forEach(o -> o.setAvailable(true));
         gamePatternDtos.sort(Comparator.comparingLong(GamePatternDto::getOrderId));
         return PageDto.of(result.getTotalElements(), page, gamePatternDtos);
     }
 
-    private List<GamePattern> availableByUser(User user){
+    private List<GamePattern> availableByUser(User user) {
         List<GamePattern> gamePatterns = gamePatternRepository.findAll();
         return gamePatterns.stream()
-                .filter(o->available(o, user))
+                .filter(o -> available(o, user))
                 .collect(Collectors.toList());
     }
 
-    private boolean available(GamePattern gamePattern, User user){
+    private boolean available(GamePattern gamePattern, User user) {
         for (ConditionParameter conditionParameter : gamePattern.getConditionParameters()) {
-            for (UserParameter userParameter: user.getUserParameters()) {
+            for (UserParameter userParameter : user.getUserParameters()) {
                 if (conditionParameter.getTitle().equals(userParameter.getTitle())
-                        && conditionParameter.getValue() > 0
-                        && !(conditionParameter.getValue().equals(userParameter.getValue())))
+                        && conditionParameter.getValue() > userParameter.getValue())
                     return false;
             }
         }
